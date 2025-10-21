9891e3f89357784f0984e03cd277b18f5e7096fa
chadd williams <will4614@pacificu.edu>
2025-10-20 21:48:40 -0700

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+################################################################################
+# File name:    main.py
+# Author:       YOUR NAME HERE
+# Date:         5/14/2025
+# Class:        CS 170
+# Assignment:   01Lab
+# Purpose:      Demonstrate Codespaces & Python
+# Hours:        1.5
+################################################################################
+
+from datetime import date
+
+print('Hello world!')
+
+print('Today is', date.today())
