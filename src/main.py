4447450a2accb83103d3ff6239b3f0b4d1fb0d4e
chadd williams <will4614@pacificu.edu>
2025-10-22 14:32:05 -0700

    Implement file reading for assignment data
    Added functionality to read assignment data from a specified file and store it in a dictionary.

--- src/main.py
+++ src/main.py
@@ -8,4 +8,36 @@
 # Hours:        1.5
 ################################################################################
 
-import sys # for sys.exit(-1)
+import sys
+
+# Files, Exceptions, Lists, Dictionaries
+DELIMITER = ','
+NAME = 0
+VALUE = 1
+dAssignments = {}
+dStudents = {}
+
+sFileName = input('Which file contains your data? ')
+
+try:
+    with open(sFileName) as fInputFile:
+        lAssignmentInfo = fInputFile.readline().split(DELIMITER)
+
+        for sAssignment in lAssignmentInfo:
+            lAssignmentData = sAssignment.split()
+            dAssignments[lAssignmentData[NAME]] = int(lAssignmentData[VALUE])
+
+            print(f'Name: {lAssignmentData[NAME]} Max VALUE: {dAssignments[lAssignmentData[NAME]]}')
+
+### WRITE PART TWO HERE
+
+#### END PART TWO
+
+### WRITE PART THREE HERE
+
+#### END PART THREE
+    print()
+except Exception as err:
+    print(err) # so you can see any errors if they occur!
+    print(f'Cannot open file {sFileName}')
+    sys.exit(-1)
